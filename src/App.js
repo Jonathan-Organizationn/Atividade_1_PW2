f1fdf79fd732e02dbe6d5383792f46cf5d2c3928
Jonathaannn <jonathanjog237@gmail.com>
2023-09-11 19:38:10 -0300

    Creating the main page header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Styles from "./Components/App.module.css";
+import Infomation from "./Components/Infomacoes/Index";
+
 function App() {
   return (
-    <div>
-      
+    <div className={Styles.Container}>
+      <div className={Styles.Box}></div>
+      <Infomation />
     </div>
   );
 }
---
+++ src/Components/Infomacoes/Index.js
@@ -0,0 +1,14 @@
+import Styles from "./Index.module.css";
+
+function Infomation() {
+  return (
+    <div className={Styles.Info}>
+      <h2>
+        Encontre <span>Fábio</span> no local de partida
+      </h2>
+      <p>Chega em 3 minutos (800 metros)</p>
+    </div>
+  );
+}
+
+export default Infomation;
