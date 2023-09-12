32ed89ea22ea33df4ad5bffb5d669a0afdfaf6b4
Jonathaannn <jonathanjog237@gmail.com>
2023-09-11 21:28:40 -0300

    Creating information about the car

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Styles from "./Components/App.module.css";
 import Infomation from "./Components/Infomacoes/Index";
+import Profile from "./Components/Motorista/Index";
 
 function App() {
   return (
     <div className={Styles.Container}>
       <div className={Styles.Box}></div>
       <Infomation />
+      <Profile />
     </div>
   );
 }
