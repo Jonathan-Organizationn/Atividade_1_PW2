1c4ef9fdc44d3e7374f7e3274b2f02576b9459a0
Jonathaannn <jonathanjog237@gmail.com>
2023-09-11 22:07:42 -0300

    Creating text input for messages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Styles from "./Components/App.module.css";
 import Infomation from "./Components/Infomacoes/Index";
+import InputText from "./Components/Input/Index";
 import Profile from "./Components/Motorista/Index";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <div className={Styles.Box}></div>
       <Infomation />
       <Profile />
+      <InputText />
     </div>
   );
 }
